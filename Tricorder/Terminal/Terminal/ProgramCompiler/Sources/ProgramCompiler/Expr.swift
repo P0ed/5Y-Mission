d54e04cb3417e127c08c69d05855ea9a031958b4
Konstantin Sukharev <poed@me.com>
2025-04-09 23:22:08 +0400

    Add leavesFirst

--- Tricorder/Terminal/Terminal/ProgramCompiler/Sources/ProgramCompiler/Expr.swift
+++ Tricorder/Terminal/Terminal/ProgramCompiler/Sources/ProgramCompiler/Expr.swift
@@ -25,48 +25,43 @@ public indirect enum TypeExpr {
 
 extension Scope {
 
-	mutating func traverse(_ transform: (inout Expr, Scope) -> Void) {
-		for idx in exprs.indices { exprs[idx].traverse(in: self, transform) }
+	mutating func traverse(leavesFirst: Bool = false, _ transform: (inout Expr, Scope) -> Bool) {
+		for idx in exprs.indices { exprs[idx].traverse(in: self, leavesFirst: leavesFirst, transform: transform) }
+	}
+	mutating func traverseAll(_ transform: (inout Expr, Scope) -> Void) {
+		traverse { e, s in transform(&e, s); return false }
+	}
+	mutating func traverseLeavesFirst(_ transform: (inout Expr, Scope) -> Void) {
+		traverse(leavesFirst: true) { e, s in transform(&e, s); return false }
 	}
 	mutating func traverseExprs(_ transform: (inout Expr, Scope) -> Void) {
-		for idx in exprs.indices { exprs[idx].traverseExprs(in: self, transform) }
+		traverse { e, s in
+			transform(&e, s)
+			return if case .funktion = e { true } else { false }
+		}
 	}
 }
 
 extension Expr {
 
-	mutating func traverse(in scope: Scope, _ transform: (inout Expr, Scope) -> Void) {
-		transform(&self, scope)
+	mutating func traverse(in scope: Scope, leavesFirst: Bool = false, transform: (inout Expr, Scope) -> Bool) {
+		if !leavesFirst, transform(&self, scope) { return }
 
 		switch self {
 		case .consti, .constu, .constf, .consts, .id, .tuple, .typDecl:
 			break
 		case .varDecl(let id, let type, var e):
-			e.traverse(in: scope, transform)
+			e.traverse(in: scope, transform: transform)
 			self = .varDecl(id, type, e)
 		case .funktion(let id, let labels, var scope):
 			scope.traverse(transform)
 			self = .funktion(id, labels, scope)
 		case .binary(let op, var lhs, var rhs):
-			lhs.traverse(in: scope, transform)
-			rhs.traverse(in: scope, transform)
+			lhs.traverse(in: scope, transform: transform)
+			rhs.traverse(in: scope, transform: transform)
 			self = .binary(op, lhs, rhs)
 		}
-	}
 
-	mutating func traverseExprs(in scope: Scope, _ transform: (inout Expr, Scope) -> Void) {
-		transform(&self, scope)
-
-		switch self {
-		case .consti, .constu, .constf, .consts, .id, .tuple, .typDecl, .funktion:
-			break
-		case .varDecl(let id, let type, var e):
-			transform(&e, scope)
-			self = .varDecl(id, type, e)
-		case .binary(let op, var lhs, var rhs):
-			transform(&lhs, scope)
-			transform(&rhs, scope)
-			self = .binary(op, lhs, rhs)
-		}
+		if leavesFirst { _ = transform(&self, scope) }
 	}
 }
--- Tricorder/Terminal/Terminal/ProgramCompiler/Sources/ProgramCompiler/Precompiler.swift
+++ Tricorder/Terminal/Terminal/ProgramCompiler/Sources/ProgramCompiler/Precompiler.swift
@@ -13,7 +13,7 @@ private extension Scope {
 
 	mutating func indexFunctions() throws {
 		var id = 0
-		traverse { xpr, scope in
+		traverseAll { xpr, scope in
 			if case .funktion(_, let labels, var fs) = xpr {
 				fs.parent = { scope }
 				xpr = .funktion(id, labels, fs)
@@ -23,15 +23,13 @@ private extension Scope {
 	}
 
 	mutating func collectCaptureList() throws {
-		traverse { xpr, scope in
+		traverseAll { xpr, scope in
 			if case .funktion(let funcId, let labels, var fs) = xpr {
 				var closure = [] as [Var]
 				fs.traverseExprs { x, xs in
-					if case let .id(name) = x {
-						if xs.local(name) == nil, let v = scope.local(name) {
-							let offset = closure.map(\.type.size).reduce(0, +)
-							closure.append(Var(offset: offset, type: v.type, name: name, selector: .closure))
-						}
+					if case let .id(name) = x, xs.local(name) == nil, let v = scope.local(name) {
+						let offset = closure.map(\.type.size).reduce(0, +)
+						closure.append(Var(offset: offset, type: v.type, name: name, selector: .closure))
 					}
 				}
 				fs.closure = closure
